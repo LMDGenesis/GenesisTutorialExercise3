2c4a6648c0be7a1ca9e0b3062a2afe57f0ef387a
LMDGenesis <ldiliberto@genesistrading.com>
2022-03-31 14:38:55 -0400

    Almost works
    Can get all the UTXOs and sort through them for all children, need to fix sending money. It sends money from each UTXO rather than calculating it.

--- main.mjs
+++ main.mjs
@@ -9,40 +9,10 @@ import axios from 'axios';
 const APIPASS = process.env.APIPASS || 'satoshi';
 const APIURL = process.env.APIURL || 'https://regtest.bitbank.cc/1';
 const regtestUtils = new RegtestUtils({ APIPASS, APIURL });
-
+const validator = (pubkey, msghash, signature) => ECPair.fromPublicKey(pubkey).verify(msghash, signature)
 const ECPair = ECPairFactory(ecc);
 const bip32 = BIP32Factory(ecc);
 const regtest = regtestUtils.network;
-
-const mnemonic = bip39.generateMnemonic(256)
-const path = "m/44'/1'/0'/0/0";
-
-const validator = (pubkey, msghash, signature) => ECPair.fromPublicKey(pubkey).verify(msghash, signature)
-
-//Generates random mnemonic
-//console.log(mnemonic)
-//Overiting for conistatncy
-const mnemonicSaved = "clip finish garbage off nice bicycle memory mouse shy multiply bonus busy client tattoo hamster gold slam lava orange pave arm grocery midnight name"
-//console.log(mnemonicSaved)//Prints Mnemon
-
-const seed = bip39.mnemonicToSeedSync(mnemonicSaved)
-//Taking a mnemonic phrase to a list of 64 numbers that stay consistant of the phrase you enter
-//console.log(seed)
-
-//Get the seed from mnemonic already made
-//Use bip32 to derive children from taht key with the path
-
-//Get the bip32 root from seed
-const root = bip32.fromSeed(seed);
-//Get the keyPair from the root
-const keyPair = root.derivePath(path);
-//console.log("KeyPair: ", keyPair)
-
-const { address } = bitcoinjs.payments.p2pkh({
-    pubkey: keyPair.publicKey,
-    network: bitcoinjs.networks.testnet,
-});
-
 
 const getTransactionsFromAddress = async (address) => {
     try {
@@ -90,19 +60,26 @@ const broadcastToTestnet = async(transaction) => {
     }
 }
 
-const sendMoney = async(amountToSend, yourAddress, addressToSendTo) => {
+const sendMoney = async(amountToSend, yourAddresses, yourKeyPairs, addressToSendTo) => {
 
-    const transactions = await getTransactionsFromAddress(address);
-    const utxos = await getUTXOFromAddress(address);
-    //console.log(utxos);
-    const miningFee = 15000
+    const miningFee = 25000
     var totalBalance = 0;
+    
+    //For each address that has a UTXO
+    for(let x = 0; x < yourAddresses.length; x++){
 
-    for (let i = 0; i < utxos.length; i++) {
-        totalBalance += utxos[i].value
+        //Get all of their UTXO's
+        var utxos = await getUTXOFromAddress(yourAddresses[x]);
+
+        //Add the balance of each of the UTXO's to the total balance to see how much you have
+        for (let i = 0; i < utxos.length; i++) {
+            totalBalance += utxos[i].value
+        }
     }
+    
 
-    console.log("Your Address: ", yourAddress)
+    console.log("Your Address: ", yourAddresses)
+    //console.log("Your KeyPairs: ", yourKeyPairs)
     console.log("Current Total Balance: ", totalBalance);
     console.log("Their Address: ", addressToSendTo);
     console.log()
@@ -114,32 +91,111 @@ const sendMoney = async(amountToSend, yourAddress, addressToSendTo) => {
     //Create Transaction You have enough money
     var psbt = new bitcoinjs.Psbt({network: bitcoinjs.networks.testnet});
     var currentBalance = 0;
-    for (let i = 0; i < utxos.length; i++) {
-        currentBalance += utxos[i].value
 
-        var transactionHash = await getFullTransactionHashFromTransactionId(utxos[i].txid)
-        var input = {hash: utxos[0].txid, index: utxos[0].vout, nonWitnessUtxo: Buffer.from(transactionHash,"hex")}
-        psbt.addInput(input);
+    for(let x = 0; x < yourAddresses.length; x++){
 
-        //console.log("Do you have enough moeny with this UTXO?")
-        if(currentBalance + miningFee > amountToSend){
-            psbt.addOutput({
-                address: addressToSendTo,
-                value: amountToSend,
-            });
+        var utxos = await getUTXOFromAddress(yourAddresses[x]);
 
-            psbt.signInput(0, ECPair.fromPrivateKey(keyPair.privateKey));
-            psbt.validateSignaturesOfInput(0, validator);
-            psbt.finalizeAllInputs();
-            await broadcastToTestnet(psbt.extractTransaction().toHex());
-            console.log("Sent " + amountToSend + " to " + addressToSendTo + " from " + yourAddress)
-            //console.log("The UTXO(s) that were used were:")
-            //console.log("If you have enough money just use that/those UTXO(s)")
-            break;
+        for (let i = 0; i < utxos.length; i++) {
+            currentBalance += utxos[i].value
+
+            var transactionHash = await getFullTransactionHashFromTransactionId(utxos[i].txid)
+            var input = {hash: utxos[0].txid, index: utxos[0].vout, nonWitnessUtxo: Buffer.from(transactionHash,"hex")}
+            psbt.addInput(input);
+
+            //Address To KeyPair
+
+
+            //console.log("Do you have enough moeny with this UTXO?")
+            if(currentBalance + miningFee > amountToSend){
+                psbt.addOutput({
+                    address: addressToSendTo,
+                    value: amountToSend,
+                });
+
+                psbt.signInput(0, ECPair.fromPrivateKey(yourKeyPairs[x].privateKey));
+                psbt.validateSignaturesOfInput(0, validator);
+                psbt.finalizeAllInputs();
+                await broadcastToTestnet(psbt.extractTransaction().toHex());
+                console.log("Sent " + amountToSend + " to " + addressToSendTo + " from " + yourAddresses[x])
+                //console.log("The UTXO(s) that were used were:")
+                //console.log("If you have enough money just use that/those UTXO(s)")
+                break;
+            }
         }
     }
 }
 
-const christiansTestNetAddress = "miXM6nD9LKsJds61B2AhsEJjNVXU6UJzRV";
+//const mnemonic = bip39.generateMnemonic(256)
+var index = 0;
+var counter  = 20; //Number of child addresses to create
+var addresses = [];
+var keyPairs = [];
+var path = "m/44'/1'/0'/0/"+index;
+var currentHeadIndex = 0;//Current head index is the index that has no transactions first
 
-sendMoney(5000, address, christiansTestNetAddress)
+const mnemonicSaved = "clip finish garbage off nice bicycle memory mouse shy multiply bonus busy client tattoo hamster gold slam lava orange pave arm grocery midnight name"
+const seed = bip39.mnemonicToSeedSync(mnemonicSaved)
+const root = bip32.fromSeed(seed);
+var transactions;
+var lastTransactionNotEmpty = false;
+
+for (let i = 0; i < counter; i++) {
+
+    //Create new adddress
+    const keyPair = root.derivePath(path);
+    keyPairs.push(keyPair);
+
+    let { address } = bitcoinjs.payments.p2pkh({
+        pubkey: keyPair.publicKey,
+        network: bitcoinjs.networks.testnet,
+    });
+
+    //Check Whether address has transactions or not
+    transactions = await getTransactionsFromAddress(address)
+    
+    if(transactions.length == 0){
+        if(lastTransactionNotEmpty){
+            currentHeadIndex = i;
+            lastTransactionNotEmpty = false;
+        }
+        lastTransactionNotEmpty = false;
+    }
+    else{
+        lastTransactionNotEmpty = true
+        counter +=1;
+    }
+
+    //Continue creating addresses until you saw 20 in a row without 
+    index+=1;
+    path = "m/44'/1'/0'/0/"+index;
+    addresses.push(address)
+}
+
+var currentAddress = addresses[currentHeadIndex];
+var currentAddressKeyPairs = keyPairs[currentHeadIndex];
+
+var UsedAddresses = addresses.slice(0,currentHeadIndex);
+var UsedAddressesKeyPairs = keyPairs.slice(0,currentHeadIndex);
+var UtxoAddresses = [];
+var UtxoKeyPairs = [];
+
+for (let i = 0; i < UsedAddresses.length; i++) {
+
+    var utxos = await getUTXOFromAddress(UsedAddresses[i])
+    //console.log(utxos)
+    
+    if(utxos.length > 0){
+        UtxoAddresses.push(UsedAddresses[i])
+        UtxoKeyPairs.push(UsedAddressesKeyPairs[i])
+    }
+}
+
+console.log("Unused Address: ",currentAddress);
+console.log("Used Addresses: ",UsedAddresses);
+console.log("Utxo Addresses: ",UtxoAddresses);
+//console.log("All Addresses: ", addresses)
+
+const joe = "mk4Y3aRXmG2UThf8UhbFzavgKkyckny2Ua";
+
+//sendMoney(500, UtxoAddresses, UtxoKeyPairs, joe)
